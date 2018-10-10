1adb9cf76cec4a5c9d119f5db48ae43fa636e334
morabrandoi <morabrandoi@gmail.com>
2018-10-10 01:52:36 -0400

    added slider and sample code

--- sketch.js
+++ sketch.js
@@ -1,7 +1,22 @@
+var slider;
+
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(600, 600);
+  slider = createSlider(0, 169, 25);
+  createP("Hello");
+  slider.position(10, height + 40);
+  slider.style('width', '80px');
+
 }
 
 function draw() {
+  var val = slider.value();
+  if (val == 169){
+    createP("abby fink hellllllla cute");
+  }
+
   background(220);
+  translate(width / 2, height / 2);
+  ellipse(0,0,val,val);
 }
